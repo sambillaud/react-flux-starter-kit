e0b1a9427a532145a5348026a1f19ffe6375914a
sam billaud <sam.billaud@gmail.com>
2017-03-09 00:32:55 +0000

    Added stores

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,21 +1,16 @@
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
+var AuthorActions = require('../../actions/authorActions');
+var AuthorStore = require('../../stores/authorStore');
 var AuthorList = require('./authorList');
 var Link = require('react-router').Link;
 
 var AuthorPage = React.createClass({
     getInitialState: function() {
         return {
-            authors: []
+            authors: AuthorStore.getAllAuthors()
         };
-    },
-
-    componentDidMount: function() {
-        if(this.isMounted()) {
-            this.setState({ authors: AuthorApi.getAllAuthors() });
-        }
     },
 
     render: function() {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
+var InitialiseActions = require('./actions/initialiseActions');
+
+InitialiseActions.initApp();
 
 Router.run(routes, function(Handler) {
    React.render(<Handler/>, document.getElementById('app'));
